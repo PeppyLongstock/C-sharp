feeaf66665206ff4029d41e2c0d2d5d58bb54885
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-11-20 14:23:37 +0300

    final

---
+++ task8/Program.cs
@@ -0,0 +1,10 @@
+int N = new int();
+Console.WriteLine("Enter a number");
+N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i = N - 1)
+{
+    i +=2;
+    Console.WriteLine(i);
+}
+Console.Write("All even numbers")
